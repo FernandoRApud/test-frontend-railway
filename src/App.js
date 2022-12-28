a89a496f688d5d7b52c695526329449b4125fc30
FernandoRApud <fernandorapud@gmail.com>
2022-12-28 20:08:31 -0300

    axios default config

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import FileButton from './FileButton';
+import axios from './axiosInit.js'
 
 function App() {
+  console.log(process.env.REACT_APP_API_URL);
   return (
     <div className="App">
       <FileButton />
